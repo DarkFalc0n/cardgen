a306105379e17a60f245f3707434fc406ba18138
DarkFalc0n <unfezant211@gmail.com>
2023-05-15 01:11:24 +0530

    style query added for rpc toggle

--- src/routes/discord.ts
+++ src/routes/discord.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
 import { createCard } from '../utils/canvas'
 import fetchDiscord from '../utils/fetchDiscord'
 import { CustomUser } from '../interface/custom'
@@ -7,16 +7,39 @@ const discordCard = Router()
 
 discordCard.get('/:id', async (req: Request, res: Response) => {
     const user = (await fetchDiscord(req.params.id)) as CustomUser
-    const cardSize = {
-        width: 800,
-        height: 200,
+    const cardSize = () => {
+        let cardHeight: number,
+            isStyleSimple: boolean,
+            isInvalid: boolean = false
+        if ('style' in req.query) {
+            if (req.query.style === 'simple') {
+                cardHeight = 200
+                isStyleSimple = true
+            } else if (req.query.style === 'full') {
+                cardHeight = 400
+                isStyleSimple = false
+            } else {
+                isInvalid = true
+                cardHeight = 200
+                isStyleSimple = true
+            }
+        } else {
+            cardHeight = 200
+            isStyleSimple = true
+        }
+        return {
+            width: 800,
+            height: cardHeight,
+            isStyleSimple: isStyleSimple,
+            isInvalid: isInvalid,
+        }
     }
-    const card = await createCard(cardSize, user)
+    const card = await createCard(cardSize(), user)
     res.contentType('text/html')
     res.send(
-        `<img src="${card}" alt="image" width="${cardSize.width / 2}" height="${
-            cardSize.height / 2
-        }" />`
+        `<img src="${card}" alt="image" width="${
+            cardSize().width / 2
+        }" height="${cardSize().height / 2}" />`
     )
 })
 
--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -2,45 +2,66 @@ import { createCanvas, Canvas, loadImage } from 'canvas'
 import { CustomUser } from '../interface/custom'
 
 const createCard = async (
-    size: { width: number; height: number },
+    style: {
+        width: number
+        height: number
+        isStyleSimple: boolean
+        isInvalid: boolean
+    },
     user: CustomUser
 ) => {
-    const canvas: Canvas = createCanvas(size.width, size.height)
+    const canvas: Canvas = createCanvas(style.width, style.height)
     const context = canvas.getContext('2d')
     const bgGradient = context.createLinearGradient(
         0,
         0,
-        size.width,
-        size.height
+        style.width,
+        style.height
     )
     bgGradient.addColorStop(0, `#8FC8EB`)
     bgGradient.addColorStop(1, '#19335A')
     context.fillStyle = bgGradient
-    context.fillRect(0, 0, size.width, size.height)
-    context.imageSmoothingEnabled = true
-    context.fillStyle = '#ffffff'
-    context.font = '35px sans-serif'
-    const truncatedUsername =
-        user.username.length > 19
-            ? `${user.username.slice(0, 16)}...`
-            : user.username
-    context.fillText(truncatedUsername, 190, 112.5)
-    const usernameWidth = context.measureText(truncatedUsername).width
-    context.fillStyle = '#bdc2ff'
-    context.font = '30px sans-serif'
-    context.fillText(`#${user.discriminator}`, 190 + usernameWidth + 5, 112.5)
+    context.fillRect(0, 0, style.width, style.height)
 
-    const userAvatar = await loadImage(
-        `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-    )
-    context.beginPath()
-    context.arc(100, 100, 62, 0, Math.PI * 2)
-    context.clip()
-    context.arc(100, 100, 60, 0, Math.PI * 2)
-    context.drawImage(userAvatar, 40, 40, 120, 120)
-    context.strokeStyle = '#ffffff'
-    context.lineWidth = 4
-    context.stroke()
+    if (style.isInvalid) {
+        context.fillStyle = '#ffffff'
+        context.font = '35px consolas'
+        const errorMsg = 'err: invalid/missing style query'
+        const errorTextWidth = context.measureText(errorMsg)
+        context.fillText(
+            errorMsg,
+            (style.width - errorTextWidth.width) / 2,
+            112.5
+        )
+    } else {
+        context.imageSmoothingEnabled = true
+        context.fillStyle = '#ffffff'
+        context.font = '35px sans-serif'
+        const truncatedUsername =
+            user.username.length > 19
+                ? `${user.username.slice(0, 16)}...`
+                : user.username
+        context.fillText(truncatedUsername, 190, 112.5)
+        const usernameWidth = context.measureText(truncatedUsername).width
+        context.fillStyle = '#bdc2ff'
+        context.font = '30px sans-serif'
+        context.fillText(
+            `#${user.discriminator}`,
+            190 + usernameWidth + 5,
+            112.5
+        )
+        const userAvatar = await loadImage(
+            `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
+        )
+        context.beginPath()
+        context.arc(100, 100, 62, 0, Math.PI * 2)
+        context.clip()
+        context.arc(100, 100, 60, 0, Math.PI * 2)
+        context.drawImage(userAvatar, 40, 40, 120, 120)
+        context.strokeStyle = '#ffffff'
+        context.lineWidth = 4
+        context.stroke()
+    }
     const url = canvas.toDataURL('image/jpeg')
     return url
 }
